d69f9259bf5b80ba6fd413437edbe25c1a3e3813
NoctisLight <flush715@outlook.com>
2026-05-04 15:10:10 +0100

    Start TS : Constant.ts

---
+++ ts/src/constant.ts
@@ -0,0 +1,11 @@
+export const WSOL_MINT = "So11111111111111111111111111111111111111112"
+export const JITOSOL_MINT = "J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn"
+
+export const JUPITER_QUOTEAPI= "https://lite-api.jup.ag/swap/v1/quote";
+
+
+// Const variables for the jupiter quotes - Will be able to parse later
+const AMOUNT_JITOSOL = 1_000_000_000;
+// Default for the moment - NOTE : Transaction will fail with jup if amount exceeds the threshold
+const slippage_bps = 50
+
